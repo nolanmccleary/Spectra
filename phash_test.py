d6316c22eba1b9b2f8a525c1f02270529f875ff9
Nolan <62817066+ncm88@users.noreply.github.com>
2025-04-14 23:01:26 -0400

    pending gpu acceleration

--- phash_test.py
+++ phash_test.py
@@ -1,30 +1,17 @@
-#TODO: Allow for non-square image input
+#TODO: Allow for non-square image input; absorb full design thus far
+#TODO: Plot hash optimization surface
+#TODO: Plot performance metrics WRT NUM_PERTURBATIONS and MAX_CYCLES; 
 
+#TODO: Add seed, inverse delta
+#TODO: CUDA Parallelism -> CUDA DCT -> CUDA PHASH -> CUDA Hashgrad
+#TODO: LPIPS
 
-#ISSUE: Small step distances result in hash gradient not being computeable
-#ISSUE: Gets a different hash but then undoes itself, likely due to not finding the appropriate gradient
-#We need to take our gradient from the LF DCT vector
-#First thing to troubleshoot is our distribution vectors -> all perturbations are either all posuitive or all negative, this should be fixed!
 
 from PIL import Image
-import imagehash
+#import imagehash
 import numpy as np
-import random
 import scipy
-
-
-
-NUM_PERTURBATIONS= 200
-IMAGE_STRING = 'sample_images/peppers.png'
-
-DCT_DIM = 8
-DCT_HFF = 4
-DCT_SIDE_LENGTH = DCT_DIM * DCT_HFF
-
-SCALE_FACTOR = 2
-STEP_SIZE = 0.01
-
-MAX_CYCLES = 100
+import math
 
 
 
@@ -35,42 +22,33 @@ def process_image(image, image_size):
 
 
 
-def generate_perturbation_vectors(image_arr):
+def generate_perturbation_vectors(image_arr, half_size):
     dim = image_arr.size
-    half = NUM_PERTURBATIONS // 2
-    '''
-    base = np.random.randn(half, dim) #Create <half> vectors of length <dim>
-    base_normalized = (base - np.min(base, axis=1, keepdims=True)) / (np.max(base, axis=1, keepdims=True) - np.min(base, axis=1, keepdims=True))
-    
-    for vector in base_normalized:
-        for value in vactor:
-            print(vector)
-
-    perturbations = np.vstack([base_normalized, -base_normalized])
-    return perturbations
-
-    '''
-    base = np.random.randn(half, dim)
+    base = np.random.randn(half_size, dim)
     perturbations = np.vstack([base, -base])
-    
-    for vector in perturbations:
-        print(vector)
-    
     return perturbations
 
 
 
-def generate_phash(pixels):
-    height = DCT_SIDE_LENGTH
-    width = DCT_SIDE_LENGTH
+def generate_phash(pixels, height, width, dct_dim):
     pixels_2d = pixels.reshape((height, width))    
     dct = scipy.fftpack.dct(scipy.fftpack.dct(pixels_2d, axis=0), axis=1)
-    #dct_kernel = dct[1 : DCT_DIM + 1, 1 : DCT_DIM + 1] #Choose this indexing as DC term contribution to image structure is minimal
-    dct_kernel = dct[:DCT_DIM, :DCT_DIM]
+    dct_kernel = dct[1 : dct_dim + 1, 1 : dct_dim + 1] #Choose this indexing as DC term contribution to image structure is minimal
+    #dct_kernel = dct[:DCT_DIM, :DCT_DIM]
     avg = np.median(dct_kernel)
     diff = dct_kernel > avg
     bitstring = ''.join(['1' if b else '0' for b in diff.flatten()])
     return hex(int(bitstring, 2))
+
+
+
+def l2_per_pixel(img1, img2, imgsize):
+    delta_arr = (img1 - img2) ** 2
+    sum = 0
+    for element in delta_arr:
+        sum += element
+    sum = math.sqrt(sum)
+    return sum / imgsize
 
 
 
@@ -82,57 +60,63 @@ def hamming_distance_hex(hex1, hex2):
 
 
 
-def perturbation_hash_delta(image, perturbation):
-    new_image = image + perturbation
-    return hamming_distance_hex(generate_phash(image), generate_phash(new_image))
-
-
-
-def image_pipeline():
-
+def phash_attack():
+    NUM_PERTURBATIONS = 3000 
+    IMAGE_STRING = 'sample_images/peppers.png'
+    DCT_DIM = 8
+    DCT_HFF = 4
+    DCT_SIDE_LENGTH = DCT_DIM * DCT_HFF
+    SCALE_FACTOR = 6                       #Higher: l2 rises quicker but solution found faster; lower, l2 rises slower but solution takes longer to discover or not discovered at all (this could result in an overall larger l2 distance as l2 increases monotonically as the program runs (statistically speaking))
+    STEP_SIZE = 0.01
+    MAX_CYCLES = 10000
+    HASH_THRESHOLD = 8
+    
     rgb_image = Image.open(IMAGE_STRING)
 
     converted_image = process_image(rgb_image, DCT_SIDE_LENGTH)
     converted_image_size = converted_image.size
 
-    initial_hash = generate_phash(converted_image)
+    initial_hash = generate_phash(converted_image, DCT_SIDE_LENGTH, DCT_SIDE_LENGTH, DCT_DIM)
 
-    perturbations = generate_perturbation_vectors(converted_image)
-    current_image = converted_image.copy()  #Numpy by default passes arrays by reference, I don't  even wanna say how much time I spent trying to debug this.
+    perturbations = generate_perturbation_vectors(converted_image, NUM_PERTURBATIONS // 2)
+    current_image = converted_image.copy()  #Numpy by passes arrays by reference; I don't even wanna say how much time I spent trying to debug this.
     total_delta = np.zeros(converted_image_size)
 
+    nruns = 0
+
     for _ in range(MAX_CYCLES):
+        nruns += 1
+        
         gradient = np.zeros(converted_image_size, dtype='float64')
 
         #TODO: Parallelize this
         for i in range(NUM_PERTURBATIONS):
             scaled_perturbation = perturbations[i] * SCALE_FACTOR
-            hash_delta = perturbation_hash_delta(current_image, scaled_perturbation) #discrete hamming delta lowers gradient sensitiivity
-            #if(hash_delta != 0):
-            #    print(initial_hash)
-            #    print(generate_phash(current_image + scaled_perturbation))
-
-            gradient += hash_delta * scaled_perturbation
+            hash_delta = hamming_distance_hex(generate_phash(current_image, DCT_SIDE_LENGTH, DCT_SIDE_LENGTH, DCT_DIM), generate_phash(current_image + scaled_perturbation, DCT_SIDE_LENGTH, DCT_SIDE_LENGTH, DCT_DIM)) #discrete hamming delta lowers gradient sensitiivity
+            gradient += hash_delta * scaled_perturbation #could possibly scale this
                 
         delta = np.sign(gradient) * STEP_SIZE
-
         current_image += delta
         total_delta += delta
-        #print(total_delta)
+
+        ham = hamming_distance_hex(generate_phash(current_image, DCT_SIDE_LENGTH, DCT_SIDE_LENGTH, DCT_DIM), initial_hash)
+        l2 = l2_per_pixel(current_image, converted_image, converted_image_size)
+        print("HAM: " + str(ham) + " L2: " + str(l2))
+
+        if ham >= HASH_THRESHOLD:
+            break
 
 
 
-    #for i in range(current_image.size):
-    #    if current_image[i] != converted_image[i]:
-    #        print(converted_image[i])
-    #        print(current_image[i])
-            
+
     print(initial_hash)
-    current_hash = generate_phash(current_image)
+    current_hash = generate_phash(current_image, DCT_SIDE_LENGTH, DCT_SIDE_LENGTH, DCT_DIM)
     print(current_hash)
     print(hamming_distance_hex(initial_hash, current_hash))
+    print(l2_per_pixel(current_image, converted_image, converted_image_size))
+    print(nruns)
 
-image_pipeline()
+phash_attack()
 
 
 
